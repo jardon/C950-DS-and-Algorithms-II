d9dc5e8c4148bf936e03eb764ea197b2ad28945d
Jarred Wilson <jarred.wilson12@gmail.com>
2020-03-09 13:38:24 -0400

    Add casting to types in distances

--- models/Distances.py
+++ models/Distances.py
@@ -15,15 +15,16 @@ with open('data/distance_address.csv') as file:
                 address_table.add(key, [row[0], row[1], key])
 
 def find_closest_destination(start, destinations):
+    start = int(start)
     closest_distance = 100.0
     closest_location = None
-    for distance in [less for less in destinations if less < start]:
-        if float(distance_data[start][distance]) < float(closest_distance) and distance in destinations:
+    for distance in [less for less in destinations if int(less) < start]:
+        if float(distance_data[start][int(distance)]) < float(closest_distance) and distance in destinations:
             closest_location = distance
-            closest_distance = distance_data[start][distance]
-    for distance in [greater for greater in destinations if greater > start]:
-        if float(distance_data[distance][start]) < float(closest_distance) and distance in destinations:
+            closest_distance = distance_data[start][int(distance)]
+    for distance in [greater for greater in destinations if int(greater) > start]:
+        if float(distance_data[int(distance)][start]) < float(closest_distance) and distance in destinations:
             closest_location = distance
-            closest_distance = distance_data[distance][start]
+            closest_distance = distance_data[int(distance)][start]
 
     return (closest_location, closest_distance)
