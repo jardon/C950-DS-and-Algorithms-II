00fa4f8719543f5f56eb11c06182f1be5b728446
Jarred Wilson <jarred.wilson12@gmail.com>
2020-03-10 20:04:10 -0400

    Implement priority lists

--- main.py
+++ main.py
@@ -16,9 +16,15 @@ truck3_distance = 0.0
 truck1_packages = [13,14,15,16,29,30,31,34,37,40,1,4,7,39,8,32]
 truck2_packages = [3,18,36,38,6,10,11,12,17,20,21,22,23,24,25,26]
 truck3_packages = [9,19,27,28,33,35,2,5]
+truck1_priority = [13,14,15,16,29,30,31,34,37,40]
+truck2_priority = [6,20,25]
+truck3_priority = []
 truck1_destinations = []
 truck2_destinations = []
 truck3_destinations = []
+truck1_priority_destinations = []
+truck2_priority_destinations = []
+truck3_priority_destinations = []
 truck1_pos = 0
 truck2_pos = 0
 truck3_pos = 0
@@ -30,6 +36,7 @@ for index in truck1_packages:
 
     if address == address2 and addressId not in truck1_destinations:
         truck1_destinations.append(addressId)
+        truck1_priority_destinations.append(addressId)
 
 for index in truck2_packages:
     address = package_list.get(str(index))[1]
@@ -38,6 +45,7 @@ for index in truck2_packages:
 
     if address == address2 and addressId not in truck2_destinations:
         truck2_destinations.append(addressId)
+        truck2_priority_destinations.append(addressId)
 
 for index in truck3_packages:
     address = package_list.get(str(index))[1]
@@ -46,9 +54,16 @@ for index in truck3_packages:
 
     if address == address2 and addressId not in truck3_destinations:
         truck3_destinations.append(addressId)
+        truck3_priority_destinations.append(addressId)
 
 while True:
-    if len(truck1_destinations) > 0:
+    if len(truck1_priority_destinations) > 0:
+        truck1_next = find_closest_destination(truck1_pos, truck1_priority_destinations)
+        truck1_pos = truck1_next[0]
+        truck1_distance += float(truck1_next[1])
+        truck1_destinations.remove(truck1_next[0])
+        truck1_priority_destinations.remove(truck1_next[0])
+    elif len(truck1_destinations) > 0:
         truck1_next = find_closest_destination(truck1_pos, truck1_destinations)
         truck1_pos = truck1_next[0]
         truck1_distance += float(truck1_next[1])
@@ -56,8 +71,14 @@ while True:
         if len(truck1_destinations) < 1:
             truck1_distance += distance_to_home(truck1_pos)
             truck1_pos = 0
-    
-    if len(truck2_destinations) > 0:
+
+    if len(truck2_priority_destinations) > 0:
+        truck2_next = find_closest_destination(truck2_pos, truck2_priority_destinations)
+        truck2_pos = truck2_next[0]
+        truck2_distance += float(truck2_next[1])
+        truck2_destinations.remove(truck2_next[0])
+        truck2_priority_destinations.remove(truck2_next[0])
+    elif len(truck2_destinations) > 0:
         truck2_next = find_closest_destination(truck2_pos, truck2_destinations)
         truck2_pos = truck2_next[0]
         truck2_distance += float(truck2_next[1])
@@ -66,7 +87,13 @@ while True:
             truck2_distance += distance_to_home(truck2_pos)
             truck2_pos = 0
 
-    if len(truck3_destinations) > 0:
+    if len(truck3_priority_destinations) > 0:
+        truck3_next = find_closest_destination(truck3_pos, truck3_priority_destinations)
+        truck3_pos = truck3_next[0]
+        truck3_distance += float(truck3_next[1])
+        truck3_destinations.remove(truck3_next[0])
+        truck3_priority_destinations.remove(truck3_next[0])
+    elif len(truck3_destinations) > 0:
         truck3_next = find_closest_destination(truck3_pos, truck3_destinations)
         truck3_pos = truck3_next[0]
         truck3_distance += float(truck3_next[1])
